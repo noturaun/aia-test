aa15bad92efe30c8f226fb046f7bf69c0e5b56cd
Muhammad Syahrul <74359025+noturaun@users.noreply.github.com>
2021-10-09 02:35:06 +0700

    change java runtime to 17

--- src/main/java/com/noturaun/flickertest/FlickertestApplication.java
+++ src/main/java/com/noturaun/flickertest/FlickertestApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
 
@@ -22,5 +24,13 @@ public class FlickertestApplication {
 				.setReadTimeout(Duration.ofMillis(5000))
 				.build();
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/feeds").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
